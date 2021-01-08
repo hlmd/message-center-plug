ff57a80c3829c5d53e081a4f58b0453f631a4524
黄连木笛 <leihuahui@outlook.com>
2021-01-08 15:13:47 +0800

    Constant.php

--- src/enum/Constant.php
+++ src/enum/Constant.php
@@ -12,18 +12,28 @@ class Constant
     const APP_TYPE_WECHAT_GZH  = 'wechat_gzh';                                     # 微信公众号
     const APP_TYPE_GOEASY_IM   = 'goeasy_im';                                      # GoeasyIm
     const APP_TYPE_BATCH_SEND  = 'batch_send';                                     # 批量发送
-    const APP_TYPE_MESSAGE     = 'message';                                        # 消息数据
 
-    # 放回状态码
-    const RETURN_CODE_OK = 1;                                                      # 成功
-    const RETURN_CODE_ERROR = 10000;                                               # 失败
-    const RETURN_CODE_SERVICE_NOT_FIND = 10001;                                    # 服务不存在
-    const RETURN_CODE_PARAM_ERROR = 10002;                                         # 参数异常
-    const RETURN_CODE_ACCESS_DISABLE = 10003;                                      # 禁止访问
-    const RETURN_CODE_THIRD_ERROR = 10004;                                         # 第三方错误
+    # App Type Id
+    const APP_TYPE_ID_ALIYUN_SMS  = 1;                                             # 阿里云短信
+    const APP_TYPE_ID_WECHAT_XCX  = 2;                                             # 微信小程序
+    const APP_TYPE_ID_WECHAT_GZH  = 3;                                             # 微信公众号
+    const APP_TYPE_ID_GOEASY_IM   = 4;                                             # GoeasyIm
 
     const PLATFORM_KEY = 'platform_key';                                           # 请求密匙
 
     # MessageFrom
     const MESSAGE_FROM_SYSTEM = 'system';                                          # 系统
+
+    # 发送状态
+    const SEND_STATUS_NOT_YET = 0;                                                 # 未发送
+    const SEND_STATUS_SUCCESS = 1;                                                 # 发送成功
+    const SEND_STATUS_ERROR = 2;                                                   # 发送失败
+
+    # 消息是否已读
+    const MESSAGE_UNREAD = 0;                                                      # 未读
+    const MESSAGE_READ = 1;                                                        # 已读
+
+    # 列表排序code
+    const ORDER_CODE_TIME_DESC = 1;                                                # 时间排倒序
+    const ORDER_CODE_TIME_ASC = 2;                                                 # 时间排顺序
 }
